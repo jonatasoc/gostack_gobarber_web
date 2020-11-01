c68408834366b9353ec2a50cb0ea29a87bf19b05
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2020-10-31 23:31:58 -0300

    Defining Glogal Styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import GlobalStyle from '../src/styles/global';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <h1>Hello World</h1>
+      <GlobalStyle />
+    </>
   );
 }
 
