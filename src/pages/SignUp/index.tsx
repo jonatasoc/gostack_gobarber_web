0e02ca55ce375251725f45aa0b1aacd89280cbb1
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2020-11-03 20:44:19 -0300

    Adding validation to fields with Yup

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import {
   FiArrowLeft, FiMail, FiUser, FiLock,
 } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
 import { Container, Content, Background } from './styles';
 import Input from '../../components/Input';
@@ -11,9 +12,21 @@ import Button from '../../components/Button';
 import logoImg from '../../assets/logo.svg';
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: object): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: object) => {
+    const schema = Yup.object().shape({
+      name: Yup.string().required('Nome obrigatório'),
+      email: Yup.string().required('E-mail obrigatório').email('Digite um e-mail válido'),
+      password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+    });
+
+    try {
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, [])
 
   return (
     <Container>
