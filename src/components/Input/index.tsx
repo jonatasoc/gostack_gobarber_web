4acdd0d7d7bf5f336528313c697ed761f0f82f15
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2020-11-04 22:02:26 -0300

    Creating Error Styleds to input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,8 +1,9 @@
 import React, { InputHTMLAttributes, useEffect, useRef, useState, useCallback } from 'react';
 import { IconBaseProps } from 'react-icons';
+import { FiAlertCircle } from 'react-icons/fi';
 import { useField } from '@unform/core';
 
-import { Container } from './styles';
+import { Container, Error } from './styles';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
@@ -36,7 +37,7 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
   }, [fieldName, registerField]);
 
   return (
-    <Container isFocused={isFocused} isFilled={isFilled}>
+    <Container isErrored={!!error} isFocused={isFocused} isFilled={isFilled}>
       { Icon && <Icon size={20} />}
       <input
         onFocus={handleInputFocus}
@@ -45,7 +46,10 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
         ref={inputRef}
         {...rest}
       />
-      {error}
+      {error && (
+        <Error>
+          <FiAlertCircle color="#c53030" size={20} />
+        </Error>)}
     </Container>
   );
 };
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -3,6 +3,7 @@ import styled, { css } from 'styled-components';
 interface ContainerProps {
   isFocused: boolean;
   isFilled: boolean;
+  isErrored: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -20,6 +21,11 @@ export const Container = styled.div<ContainerProps>`
     & + div {
       margin-top: 8px;
     }
+
+    ${props => props.isErrored
+    && css`
+      border-color: #c53030;
+    `}
 
     ${props => props.isFocused
     && css`
@@ -45,5 +51,13 @@ export const Container = styled.div<ContainerProps>`
 
   svg {
     margin-right: 16px;
+  }
+`;
+
+export const Error = styled.div`
+  height: 20px;
+  margin-left: 16px;
+  svg {
+    margin: 0;
   }
 `;
