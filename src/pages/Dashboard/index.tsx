348e1b5f77c30da069b97fb86e23485aaa88aa98
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2021-02-19 20:14:28 -0300

    Finish Header

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -3,8 +3,11 @@ import { Container, Header, HeaderContent, Profile } from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { FiPower } from 'react-icons/fi';
+import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const { signOut, user } = useAuth();
+
   return (
     <Container>
       <Header>
@@ -12,16 +15,13 @@ const Dashboard: React.FC = () => {
           <img src={logoImg} alt="GoBarber" />
 
           <Profile>
-            <img
-              src="https://avatars.githubusercontent.com/u/16616459?s=460&u=30d354e539a4bd253a48409b63638f83d6f2f4f4&v=4"
-              alt="Jonatas de Oliveira"
-            />
+            <img src={user.avatar_url} alt="Jonatas de Oliveira" />
             <div>
               <span>Bem-vindo,</span>
-              <strong>Jonatas de Oliveira Coelho</strong>
+              <strong>{user.name}</strong>
             </div>
           </Profile>
-          <button type="button">
+          <button type="button" onClick={signOut}>
             <FiPower />
           </button>
         </HeaderContent>
