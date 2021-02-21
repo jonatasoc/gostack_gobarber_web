364c1e80ce85c2747cf5a4bbc6c9d074ada0ae3b
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2021-02-20 22:11:12 -0300

    Add api.defaults headers on auth hook when the token is generated. Connect DatePicker Calendar with the API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import DayPricker, { DayModifiers } from 'react-day-picker';
 import { isWeekend } from 'date-fns';
 import 'react-day-picker/lib/style.css';
@@ -19,23 +19,63 @@ import {
 import logoImg from '../../assets/logo.svg';
 import { FiPower, FiClock } from 'react-icons/fi';
 import { useAuth } from '../../hooks/auth';
+import api from '../../services/api';
+
+interface MonthAvailabilityItem {
+  day: number;
+  available: boolean;
+}
 
 const Dashboard: React.FC = () => {
   const [selectedDate, setSelectedDate] = useState(() => {
+    // Check if is weekend. This way the day on calendar will not be automatically checked when calendar is opened on weekends days
     if (isWeekend(new Date())) {
       return undefined;
     } else {
       return new Date();
     }
   });
+  const [currentMonth, setCurrentMonth] = useState(new Date());
+  const [monthAvailability, setMonthAvailability] = useState<
+    MonthAvailabilityItem[]
+  >([]);
 
+  const { signOut, user } = useAuth();
   const handleDateChange = useCallback((day: Date, modifiers: DayModifiers) => {
     if (modifiers.available) {
       setSelectedDate(day);
     }
   }, []);
 
-  const { signOut, user } = useAuth();
+  const handleMonthChange = useCallback((month: Date) => {
+    setCurrentMonth(month);
+  }, []);
+
+  useEffect(() => {
+    api
+      .get(`/providers/${user.id}/month-availability`, {
+        params: {
+          year: currentMonth.getFullYear(),
+          month: currentMonth.getMonth() + 1,
+        },
+      })
+      .then(response => {
+        setMonthAvailability(response.data);
+      });
+  }, [currentMonth, user.id]);
+
+  const disabledDays = useMemo(() => {
+    const dates = monthAvailability
+      .filter(monthDay => monthDay.available === false)
+      .map(monthDay => {
+        const year = currentMonth.getFullYear();
+        const month = currentMonth.getMonth();
+
+        return new Date(year, month, monthDay.day);
+      });
+
+    return dates;
+  }, [currentMonth, monthAvailability]);
 
   return (
     <Container>
@@ -131,7 +171,7 @@ const Dashboard: React.FC = () => {
         <Calendar>
           <DayPricker
             fromMonth={new Date()}
-            disabledDays={[{ daysOfWeek: [0, 6] }]}
+            disabledDays={[{ daysOfWeek: [0, 6] }, ...disabledDays]}
             weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
             modifiers={{
               available: {
@@ -154,6 +194,7 @@ const Dashboard: React.FC = () => {
             ]}
             onDayClick={handleDateChange}
             selectedDays={selectedDate}
+            onMonthChange={handleMonthChange}
           />
         </Calendar>
       </Content>
