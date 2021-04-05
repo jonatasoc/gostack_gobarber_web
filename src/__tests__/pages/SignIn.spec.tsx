e296a37591a0cee4818a578e27303806a859dba8
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2021-04-05 17:23:50 -0300

    Put API URL on a environment variable

--- src/__tests__/pages/SignIn.spec.tsx
+++ src/__tests__/pages/SignIn.spec.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import SignIn from '../../pages/SignIn';
 
+// O mock é necessário aqui para passar para os testes coisas que estão vindo de outras libs
 jest.mock('react-router-dom', () => {
   return {
     useHistory: jest.fn(),
@@ -13,6 +14,8 @@ describe('SignIn Page', () => {
   it('should be able to sign in', () => {
     const { debug } = render(<SignIn />);
 
+    // Debug é uma função retornada pelo método render que
+    // fazer um console.log do HTML retornado pelo componente
     debug();
   });
 });
